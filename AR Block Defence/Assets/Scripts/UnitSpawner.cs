33a314c7c32a8b6887e77211661d5ebb916a275d
emiven <emiven@hotmail.no>
2018-10-03 14:49:10 +0200

    First comment
    importing unity project folder to github

---
+++ AR Block Defence/Assets/Scripts/UnitSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour {
+
+    public GameObject unit;
+    public int spawnInterval;
+
+    private void spawnUnit()
+    {
+
+    }
+	void Start ()
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+}
